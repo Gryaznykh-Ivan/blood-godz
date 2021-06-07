d1dec7e94f23118f5d31297c90d2ec50125aad13
gryaznykh.ivan <gryaznykh.ivan@gmail.com>
2021-06-07 18:06:24 +0300

    itemsCard cursor-pointer

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ItemCard() {
     return (
-        <div className="relative w-full">
-            <div className="p-4 bg-gray-900 rounded-xl flex flex-col">
-                <div className="bg-uncommon rounded-xl mb-4">
+        <div className="relative w-full cursor-pointer">
+            <div className="p-4 bg-gray-900 rounded-2xl flex flex-col">
+                <div className="bg-uncommon rounded-2xl mb-4">
                     <img className="m-auto" src="/static/images/skins/skin.png" alt="" />
                 </div>
                 <div className="text-center text-gray-500">Glock-17</div>
